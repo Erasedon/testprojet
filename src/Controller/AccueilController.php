d6bbabd05b79f5ff4ec3808ce44a37ef567080d0
yanis.pirlet <yanis.pirlet@gmail.com>
2022-07-27 11:37:07 +0200

    webpack

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AccueilController extends AbstractController
 {
     #[Route('/accueil', name: 'app_accueil')]
-    public function index(): Response
+    public function index(UserRepository $userRepo): Response
     {
+
+        $users = $userRepo->findAll();
+        foreach($users as $user) {
+            dump($user->getEmail());
+        };
+
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
+            'users' => $users,
         ]);
     }
 }
